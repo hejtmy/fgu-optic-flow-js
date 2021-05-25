b76d287c839bd5f5b73400457fef54b3e6ff0dc0
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-05-25 19:33:33 +0200

    Changes how the central rect is constructed

--- animejs/stars.js
+++ animejs/stars.js
@@ -2,26 +2,44 @@ var numberOfEls = 500;
 const speed = 1000;
 const radius = 500;
 const starSize = 10;
-let sizeVar = ["5px", "25px"];
+let sizeVar = [5, 25];
 var midScreenX = window.innerWidth / 2;
 var midScreenY = window.innerHeight / 2;
 var oldradius = Math.sqrt(midScreenX * midScreenX + midScreenY * midScreenY);
 var stars = document.getElementById('stars');
 
-prepareStars = function(){
+prepareStarsCircle = function(number, color = 'white', size = 10){
     let elements = [];
-    for (var i = 0; i < numberOfEls; i++) {
-        var hue = Math.round(360 / numberOfEls * i);
+    for (var i = 0; i < number; i++) {
         var angle = Math.random() * Math.PI * 2;
         var el = document.createElement('div');
         el.classList.add('particle', 'star');
-        el.style.backgroundColor = 'white';
-        el.style.width = starSize + 'px';
-        el.style.height = starSize + 'px';
+        el.style.backgroundColor = color;
+        el.style.width = size + 'px';
+        el.style.height = size + 'px';
         var angle = Math.random()*Math.PI*2;
         var dist = Math.random()*radius;
-        x = Math.cos(angle)*dist + midScreenX + 'px'; 
-        y = Math.sin(angle)*dist + midScreenY + 'px'; 
+        x = Math.cos(angle)*dist + midScreenX + 'px';
+        y = Math.sin(angle)*dist + midScreenY + 'px';
+        el.style.left = x;
+        el.style.bottom = y;
+        elements.push(el);
+        stars.appendChild(el);
+    }
+    return elements;
+}
+
+prepareStarsRect = function(number, color = 'white', size = [2,10]){
+    let elements = [];
+    for (var i = 0; i < numberOfEls; i++) {
+        var el = document.createElement('div');
+        el.classList.add('particle', 'star');
+        el.style.backgroundColor = color;
+        let s = Math.random()*(size[1] - size[0]) + size[0] + 'px';
+        el.style.width = s;
+        el.style.height = s;
+        x = Math.random()*window.innerWidth;
+        y = Math.random()*window.innerHeight
         el.style.left = x;
         el.style.bottom = y;
         elements.push(el);
@@ -34,7 +52,8 @@ randomMovement = function(elements){
     for(var i = 0; i < elements.length; i++){
         anime({
             targets: elements[i],
-            translateX: 100,
+            translateX: Math.random()*100,
+            translateY: Math.random()*100,
             duration: speed,
             easing: 'linear',
             loop: true
@@ -42,6 +61,18 @@ randomMovement = function(elements){
     }
 }
 
+horizontalMovement = function(elements, direction = "left"){
+    let transX = direction == "left" ? -100 : 100;
+    for(var i = 0; i < elements.length; i++){
+        anime({
+            targets: elements[i],
+            translateX: transX,
+            duration: speed,
+            easing: 'linear',
+            loop: true
+        });
+    }
+}
 
 createCircle = function(x, y, radius, fill = "#646464"){
     let svgContainer = document.createElement('div');
@@ -106,7 +137,7 @@ createBackground = function(x, y, radius, fill = '#646464'){
     return svgContainer;
 }
 
-let elements = prepareStars();
-randomMovement(elements);
-//stars.appendChild(createCircle(midScreenX, midScreenY, 100));
-stars.appendChild(createBackground(midScreenX, midScreenY, radius));
+let elements = prepareStarsRect();
+//randomMovement(elements);
+
+horizontalMovement(elements, "right");
