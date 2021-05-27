de98512adced38742f67a7bf05d5b57bc3f23b52
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-05-27 02:06:04 +0200

    Adds horizontal and other movements with keeping stars in canvas

--- stars.js
+++ stars.js
@@ -15,14 +15,16 @@ window.requestAnimFrame = (function(){return  window.requestAnimationFrame})();
 var canvas = document.getElementById("space");
 var c = canvas.getContext("2d");
 
-var ZAxis = 2000;
+var depth = 2000;
 
 var numStars = 300;
 var radius = '0.'+ Math.floor(Math.random() * 9) + 1;
 var focalLength = canvas.width * 2;
 var warp = 0;
 var centerX, centerY;
-var speed = 5000;
+var speed=  5;
+var horizontalSpeed = 1;
+var depthSpeed = 5;
 var starSize = 5;
 
 var stars = [], star;
@@ -52,10 +54,10 @@ function initializeStars(){
     star = {
       x: Math.random() * canvas.width,
       y: Math.random() * canvas.height,
-      z: Math.random() * ZAxis,
+      z: Math.random() * depth,
       o: '0.'+ Math.floor(Math.random() * 99) + 1,
-      dir_x: (Math.random() * canvas.width - canvas.width/2)/speed,
-      dir_y: (Math.random() * canvas.height - canvas.height/2)/speed,
+      dir_x: (Math.random() + 0.1) * horizontalSpeed,
+      dir_y: (Math.random() + 0.1) * horizontalSpeed
     };
     stars.push(star);
   }
@@ -83,25 +85,37 @@ function moveRandom(stars){
         star = stars[i];
         star.x += star.dir_x;
         star.y += star.dir_y;
+        star = keepStarInCanvas(star);
+        stars[i] = star;
     }
     return stars;
 }
 
 function moveHorizontal(stars, direction){
     for(i = 0; i < stars.length; i++){
-        star = stars[i];
+        var star = stars[i];
         star.x += (((direction == FlowDirection.horizontalleft)*2)-1) * Math.abs(star.dir_x);
+        stars[i] = keepStarInCanvas(star);
     }
     return stars;
 }
 
+function keepStarInCanvas(star){
+    if(star.x < 0){star.x = canvas.width;}
+    if(star.x > canvas.width){star.x = 0;}
+    if(star.y < 0){star.y = canvas.height;}
+    if(star.y > canvas.height){star.y = 0;}
+    return star;
+}
+
 function moveRadial(stars, direction){
     for(i = 0; i < stars.length; i++){
-        star = stars[i];
-        star.z += (direction == FlowDirection.radialin)*2-1;
-        if(star.z <= 0 || star.z >= ZAxis){
+        var star = stars[i];
+        star.z = ((direction == FlowDirection.radialin)*2-1) * speed;
+        if(star.z <= 0 || star.z >= depth){
             star.z = 0;
         }
+        stars[i] = star;
     }
     return stars;
 }
@@ -127,13 +141,13 @@ function drawStars(){
       initializeStars();
     }
     if(warp == 0){
-      c.fillStyle = "rgba(0,10,20,1)";
-      c.fillRect(0, 0, canvas.width, canvas.height);
+        c.fillStyle = "rgba(0,10,20,1)";
+        c.fillRect(0, 0, canvas.width, canvas.height);
     }
     c.fillStyle = "rgba(209, 255, 255, " + radius + ")";
     switch(OpticFlowSettings.CurrentFlowDirection){
         case FlowDirection.random:
-            drawRandom(stars);
+            drawStars2(stars);
             break;
         case FlowDirection.radialin:
         case FlowDirection.radialout:
@@ -141,7 +155,7 @@ function drawStars(){
             break;
         case FlowDirection.horizontalleft:
         case FlowDirection.horizontalright:
-            drawRandom(stars);
+            drawStars2(stars);
             break;
 
     }
@@ -161,25 +175,22 @@ function drawRadial(stars){
     }
 }
 
-function drawHorizontal(){
-
-}
-
-function drawRandom(stars){
-    for(i = 0; i < numStars; i++){
-        star = stars[i];
-        pixelX = star.x;
-        pixelY = star.y
-        pixelRadius = starSize;
-        c.fillRect(pixelX, pixelY, pixelRadius, pixelRadius);
+function drawStars2(stars){
+    for(i = 0; i < stars.length; i++){
+        var star = stars[i];
+        var pixelRadius = starSize * ((depth - star.z) / depth);
+        c.fillRect(star.x, star.y, pixelRadius, pixelRadius);
         c.fillStyle = "rgba(209, 255, 255, " + star.o + ")";
     }
 }
 
+function drawHorizontal(){
+
+}
 // BUTTONS -------------------
 
 document.getElementById('trace').addEventListener("click", function(e){
-    window.warp = window.warp==1 ? 0 : 1;
+    window.warp = window.warp == 1 ? 0 : 1;
     initializeStars();
     window.c.clearRect(0, 0, window.canvas.width, window.canvas.height);
 });
