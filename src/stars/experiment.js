7682842c741885e0340a6a08f022fb3a3b09a321
Lukáš Hejtmánek <hejtmy@gmail.com>
2021-06-18 21:50:33 +0200

    Adds basic experiment control

--- src/stars/experiment.js
+++ src/stars/experiment.js
@@ -1,7 +1,89 @@
+import { StarsController, FlowDirection } from "./stars.js";
+
+const TrialSettings = {
+    duration: 1000,
+    movementType: 0,
+}
+
+const ExperimentSettings = {
+    name: "NULL",
+    version: "1.0",
+    duration: 1000,
+    canvasSize: {x:300, y:300},
+    trials: [],
+}
+
 const OpticFlowExperiment = {
-    init: function(div){
-        this.expCanvas = div.getContext('2d');
+    starsControler: null,
+    iTrial: 0,
+    settings: null,
+    currentTrial: null,
+    trialTimeout: null,
+    running: false,
+    
+    init: function(settingsObj, canvas){
+        this.settings = this.parseSettings(settingsObj);
+        this.starsControler = Object.create(StarsController);
+        this.starsControler.initialize(canvas);
+    },
+
+    parseSettings: function(settings){
+        var settings = Object.assign(ExperimentSettings, settings);
+        // This doesn't work, it only copies over the fist element :/
+        //settings.trials = settings.trials.map((element) => Object.assign(TrialSettings, element));
+        return settings;
+    },
+    
+    loadSettings: function(){
+        this.settings
+    },
+
+    startExperiment: function(){
+        this.iTrial = -1;
+        this.starsControler.start();
+        this.running = true;
+        this.nextTrial();
+    },
+
+    pause: function(){
+        
+    },
+
+    finishExperiment: function(){
+        this.running = false;
+        this.starsControler.stop();
+    },
+
+    nextTrial: function(){
+        this.iTrial += 1;
+        this.currentTrial = this.settings.trials[this.iTrial];
+        this.startTrial();
+    },
+
+    startTrial: function(){
+        console.log("trial starting" + this.iTrial);
+        console.log(this.currentTrial.movementType);
+        //setup stars copntroller
+        this.starsControler.setFlowDirection(this.currentTrial.movementType);
+        // add timeout
+        this.trialTimeout = setTimeout(this.finishTrial.bind(this), this.currentTrial.duration);
+    },
+
+    finishTrial: function(){
+        if(this.checkLastTrial(this.iTrial, this.settings)){
+            this.finishExperiment();
+            return;
+        }
+        this.nextTrial();
+    },
+
+    logTrial: function(){
+
+    },
+
+    checkLastTrial: function(iTrial){
+        return this.iTrial >= this.settings.trials.length - 1;
     }
 }
 
-export default OpticFlowExperiment;
+export {OpticFlowExperiment, ExperimentSettings, TrialSettings};
