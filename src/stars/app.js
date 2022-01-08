82d0aa2b06691cb0c9874766c471753515a11ea9
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-01-08 11:30:02 +0100

    Log name in better format

--- src/stars/app.js
+++ src/stars/app.js
@@ -79,7 +79,8 @@ document.getElementById("btn-save-log").addEventListener('click', (e) => {
     var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
     var downloadAnchorNode = document.createElement('a');
     downloadAnchorNode.setAttribute("href",     dataStr);
-    downloadAnchorNode.setAttribute("download", "trialData.json");
+    let filename = new Date(parseInt(fileDropdown.value)).toISOString() + "_opticflow.json";
+    downloadAnchorNode.setAttribute("download", filename);
     document.body.appendChild(downloadAnchorNode); // required for firefox
     downloadAnchorNode.click();
     downloadAnchorNode.remove();
